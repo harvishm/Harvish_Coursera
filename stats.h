4d849fc8fd10304b49eae862ce8a1a535006924a
harvishm <harvishmehta@yahoo.com>
2024-09-29 00:44:43 -0600

    trying to uploade to remote site

--- stats.h
+++ stats.h
@@ -23,21 +23,75 @@
 
 /* Add Your Declarations and Function Comments here */ 
 
-void print_statistics() // Print Statistics of array min, max, and mean 
+//void print_statistics(); // Print Statistics of array min, max, and mean 
 
-void print_array(int array[], int size) //Given an array of data and a length, prints the array to the screen
+//Given an array of data and a length, prints the array to the screen
+void print_array(char array[], int size) 
+{
+	for(int i = 0; i < size; i++)
+		{
+			printf("%s ",array[i]);
+		}
+}
 
-float find_median(int array[], int size) //Given an array of data and a length, returns the median value
-
-int find_mean(int array[], int size) //Given an array of data and a length, returns the mean value
-
-int find_maximum(int array[], int size) //Given an array of data and a length, returns the max
-
-int find_minimum(int array[], int size) //Given an array of data and a length, returns the min
-
-int sort_array(int array[], int size) //Given an array of data and a length, returns the largest to smallest
+//Given an array of data and a length, returns the median value
+float find_median(char array[], int size) 
+{
 
 
+}
+
+//Given an array of data and a length, returns the mean value
+float find_mean(char array[], int size) 
+{
+	float sum = 0;
+	for(int i = 0; i < size; i++)
+		{
+			sum = sum + (int)array[i];	
+		}
+	float mean = sum/size;
+	return mean;
+
+}
+
+ //Given an array of data and a length, returns the max
+int find_maximum(char array[], int size)
+{
+	int max = 0;
+	for(int i = 0; i < size; i++)
+		{
+			if (max <= (int)array[i])
+				max = (int)array[i];
+		}
+	return max;
+}
+int find_minimum(char array[], int size) //Given an array of data and a length, returns the min
+{
+	int min = 0;
+	for(int i = 0; i < size; i++)
+		{
+			if (min >= (int)array[i])
+				min = (int)array[i];
+		}
+	return min;
+}
+
+int sort_array(char array[], int size) //Given an array of data and a length, returns the largest to smallest
+
+{
+	int sortedArray[size];
+
+	for(int m = 0; m < size; m++)
+		{
+
+			for(int b = 0; b < size; b++)
+				{
+	
+				}
+
+		}
+	return sortedArray;
+}
 
 #endif /* __STATS_H__ */
 
